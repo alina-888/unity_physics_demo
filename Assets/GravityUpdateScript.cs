5e28927e73a2e7db3279321bab9764ee4cfe2612
Alexander Khitev <akhitev@cloud.upwork.com>
2019-12-25 14:08:41 +0300

    new changes

--- Assets/GravityUpdateScript.cs
+++ Assets/GravityUpdateScript.cs
@@ -1,29 +1,72 @@
+// импортируем библиотеки
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using System;
 
+// декларируем класс
 public class GravityUpdateScript : MonoBehaviour
 {
-	public Slider myslider;
+	// задаем константы
+	const float EARTH_G = 9.8f;
+ 	const float MOON_G = 1.6f;
+	const float SPACE_G = 0f;
+ 
+	    	
+	// переменные компонентов UI
+	public Slider mySlider;
 	public Text gravityText;
-        
+        public Dropdown myDropdown;
 
     // Start is called before the first frame update
+    // стандартный метод Start вызывается перед первым обновлением кадра
     void Start()
     {
-	    Debug.Log("I am alive!");
-     			myslider.onValueChanged.AddListener (delegate {ChangeGravity();});
+	
+	Debug.Log("Hello, world!");            
+	
+
+	// добавляем Listeners(слушателей) для Slider.onValueChanged
+	mySlider.onValueChanged.AddListener (delegate {ChangeGravity();});
+	// добавляем Listeners(слушателей) для Dropdown.onValueChanged
+	myDropdown.onValueChanged.AddListener(delegate {DropdownChooseGravity(myDropdown);});
+
+	mySlider.value = EARTH_G;
         		
     }
 
 
 
+// задаем метод для dropdown
+ public void DropdownChooseGravity(Dropdown d) {
+	int index = d.value;
+		
+      switch (index)
+      {
+    	case 0:
+               //Земля
+	       mySlider.value = EARTH_G;
+		break;
+        case 1:
+	      //Луна	
+              mySlider.value = MOON_G;
+		break;
+          case 2:
+	      //Космос
+              mySlider.value = SPACE_G;
+              break;
+          default:
+              break;
+      }
+}
 
-public void ChangeGravity() {
- 	gravityText.text = "Gravity :" + Math.Round(myslider.value);
-	Physics.gravity = new Vector3(0, -myslider.value, 0);
+// задаем метод для изменения гравитации 
+ public void ChangeGravity() {
+	// обновляем текст
+ 	gravityText.text = "Gravity :" + Math.Round(mySlider.value);
+	// изменяем гравитацию в физическом движке	
+ 	Physics.gravity = new Vector3(0, -mySlider.value, 0);
 }
 
 
@@ -31,7 +74,7 @@ public void ChangeGravity() {
     // Update is called once per frame
     void Update()
     {
-	   // Debug.Log("slider: " +  myslider.value);
+	   // Debug.Log("slider: " +  mySlider.value);
         
     }
 }
